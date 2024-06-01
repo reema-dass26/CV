f7e1f8b76086ad9d1e756480706bb13fe36a07d1
reema-dass26 <reemaworkmail@gmail.com>
2024-06-01 18:58:12 +0200

    .

--- contact.php
+++ contact.php
@@ -1,28 +1,36 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+    // Check if all form fields are set and not empty
+    if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"]) &&
+        !empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"])) {
+        
+        // Sanitize form input to prevent injection
+        $name = htmlspecialchars($_POST["name"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $message = htmlspecialchars($_POST["message"]);
 
-    // Set recipient email address
-    $to = "reemaworkmail@gmail.com";
+        // Set recipient email address
+        $to = "reemaworkmail@gmail.com";
 
-    // Set email subject
-    $subject = "New message from $name";
+        // Set email subject
+        $subject = "New message from $name";
 
-    // Compose email message
-    $email_message = "Name: $name\n";
-    $email_message .= "Email: $email\n";
-    $email_message .= "Message:\n$message";
+        // Compose email message
+        $email_message = "Name: $name\n";
+        $email_message .= "Email: $email\n";
+        $email_message .= "Message:\n$message";
 
-    // Set headers
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+        // Set additional headers
+        $headers = "From: $name <$email>\r\n";
+        $headers .= "Reply-To: $email\r\n";
+        $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
 
-    // Send email
-    if (mail($to, $subject, $email_message, $headers)) {
-        echo "success";
+        // Send email
+        if (mail($to, $subject, $email_message, $headers)) {
+            echo "success";
+        } else {
+            echo "error";
+        }
     } else {
         echo "error";
     }
